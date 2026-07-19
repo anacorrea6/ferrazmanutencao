bc478def9bac293289d3230bb777539341c7af73
Ana  Corrêa <ana.j.ferraz6@aluno.senai.br>
2026-07-19 15:59:02 -0300

    .

---
+++ src/script/script.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = '#fca311';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.color = '#fff';
+    });
+});
